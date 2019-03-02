bcdc296288db0b158373a33d2c125ab7269ddf4d
Bibek <bibekmanshrestha@gmail.com>
2019-03-01 23:28:42 -0500

    add basic dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './plugins/vuelidate'
+import './plugins/vue2-filters'
+
+
+import { version } from '../package.json'
+console.log(`App version: ${version}`)
 
 Vue.config.productionTip = false
 
