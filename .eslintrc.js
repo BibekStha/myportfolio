4373427e4c1fb574b3e4b9b1c9be7a37c19b58c2
Bibek <bibekmanshrestha@gmail.com>
2019-03-19 11:22:03 -0400

    fix issue with admin access problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-multiple-empty-lines': [2, { max: 2 }]
+    'no-multiple-empty-lines': [2, { max: 2 }],
   },
   parserOptions: {
     parser: 'babel-eslint'
